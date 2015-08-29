0d55efba089685e82b1542c351b1d4c59d07f440
Pea <pea@misfist.com>
2015-08-29 17:55:48 -0400

    Further development of the angular store; created review controller that saves reviews and custom directive for reusable snippet

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@
         }
     } );
 
-    app.controller('GalleryController', function() {
+    app.controller( 'GalleryController', function() {
         this.current = 0;
 
         this.setCurrent = function(imageNum) {
@@ -30,37 +30,103 @@
                 this.current = 0;
             }
         };
-    });
+    } );
+
+    app.controller( 'ReviewController', function() {
+        this.review = {};
+
+        this.addReview = function(product) {
+            this.review.createdOn = Date.now();
+            product.reviews.push( this.review );
+            // Clear review
+            this.review = {};
+        }
+
+    } );
+
+    app.directive( 'productTitle', function() {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/product-title.html'
+        };
+
+    } );
+
+    app.directive( 'productDescription', function() {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/product-description.html'
+        };
+
+    } );
 
     var gems = [
         { 
             name: 'Azurite', 
             price: 2.95,
-            description: '',
+            description: 'This is a description.',
             shine: '',
             images: [
                 'assets/images/above-ground-pool.jpg',
                 'assets/images/black-cowboy.jpg'
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: 'This is great!!',
+                    author: 'pea@misfist.com'
+                },
+                {
+                    stars: 4,
+                    body: 'Pretty nice',
+                    author: 'misfist@yahoo.com'
+                }
             ]
         },
         { 
             name: 'Bloodstone',
             price: 5.95,
-            description: '',
+            description: 'Yeah, so it is a bloodstone. Need I say more?',
             shine: '',
             images: [
                 'assets/images/above-ground-pool.jpg',
                 'assets/images/black-cowboy.jpg'
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: '',
+                    author: ''
+                },
+                {
+                    stars: 4,
+                    body: '',
+                    author: ''
+                }
             ]
         },
         { 
             name: 'Zircon',
             price: 3.95,
-            description: '',
+            description: 'Don\'t you know what a Zircon is?',
             shine: '',
             images: [
                 'assets/images/above-ground-pool.jpg',
                 'assets/images/black-cowboy.jpg'
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: '',
+                    author: ''
+                },
+                {
+                    stars: 4,
+                    body: '',
+                    author: ''
+                }
             ]
         }
     ];
